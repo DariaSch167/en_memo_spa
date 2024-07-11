8301ef70f8502e0cf6db990af6df009de7087609
Daria <scheglova.daria@gmail.com>
2024-07-12 00:34:54 +0300

    add test state array cardline lineElEdit

--- src/components/MainListOfCards/LineElemEdit.jsx
+++ src/components/MainListOfCards/LineElemEdit.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./cardLine.css";
 
 function LineElemEdit(props) {
+  const [state, setState] = useState({
+    english: props.english,
+    transcription: props.transcription,
+    russian: props.russian,
+    tags: props.tags,
+  });
+
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+
+  // const handleChange = (e) => {
+  //   const value = e.target.value;
+  //   setState({
+  //     ...state,
+  //     [e.target.name]: value,
+  //   });
+  // };
+
   return (
     <React.Fragment>
       <div className={props.className}>
@@ -9,22 +28,26 @@ function LineElemEdit(props) {
         <input
           className="list__line__word-en"
           type="text"
-          value={props.english}
+          value={state.english}
+          onChange={handleChange}
         />
         <input
           className="list__line__word-transcription"
           type="text"
-          value={props.transcription}
+          value={state.transcription}
+          onChange={handleChange}
         />
         <input
           className="list__line__word-ru"
           type="text"
-          value={props.russian}
+          value={state.russian}
+          onChange={handleChange}
         />
         <input
           className="list__line__word-tags"
           type="text"
-          value={props.tags}
+          value={state.tags}
+          onChange={handleChange}
         />
       </div>
     </React.Fragment>
